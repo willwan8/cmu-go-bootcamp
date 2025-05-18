7b87fccec8067d89bcd1b6af08e9929ab7c37a0a
willwan8 <williamwang1562@gmail.com>
2025-05-18 11:58:31 -0500

    completed For Loops in Go code along (changes made in loops folder)

--- src/loops/main.go
+++ src/loops/main.go
@@ -1,5 +1,5 @@
 /*
-// Helpful pseudocode for this lesson
+// Helpful pseudocode for this lesson (while loops)
 Factorial(n)
   p ← 1
   i ← 1
@@ -15,6 +15,15 @@ EuclidGCD(a,b)
     else
       b ← b − a
   return a 
+*/
+
+/*
+// Helpful pseudocode for this lesson (for loops)
+AnotherFactorial(n)
+  p ← 1
+  for every integer i between 1 and n
+    p ← p·i
+  return p
 */
 
 package main
@@ -34,13 +43,97 @@ func main() {
 	fmt.Println(Factorial(40))
 	fmt.Println(Factorial(45))
 	fmt.Println(Factorial(300))
+
+	var count uint = 10
+	for ; count >= 0; count-- {
+		fmt.Println(count)
+		// hidden count--
+		// integer underflow, once we subtract 1 from 0, the unsigned integer will go to the largest possible unsigned integer as a negative uint doesn't exist
+		// therefore, this is an infinite loop!
+	}
+
+	fmt.Println("Blast off!")
+}
+
+// AnotherFactorial takes as input an integer n and returns n!
+func AnotherFactorial(n int) int {
+	if n < 0 {
+		panic("Error: negative input given to AnotherFactorial().")
+	}
+	p := 1
+
+	// the "for" keyword is also used for for loops
+	for i := 1; i <= n; i++ {
+		// i := 1 is called the "initialization step"
+		// i <= n is called the "condition"
+		// i++ is called the "postcondition"
+
+		p = p * i
+
+		// there is a hidden line of code: i++
+	}
+
+	// this will not work as i is out of scope (its scope is only in the for loop)
+	//fmt.Println("i is currently", i)
+
+	return p
+}
+
+// SumEven takes as input an integer k and returns the sum of all even positive integers up to and (possibly) including k.
+func SumEven(k int) int {
+	if k < 0 {
+		panic("Error: negative input given to SumEven().")
+	}
+
+	sum := 0
+
+	// for every even integer j between 2 and k, add j to sum
+	for j := 2; j <= k; j += 2 {
+		// j must be even
+		sum += j
+	}
+
+	return sum
+}
+
+func YetAnotherFactorial(n int) int {
+	if n < 0 {
+		panic("Error: negative input given to YetAnotherFactorial().")
+	}
+
+	p := 1
+
+	for i := n; i >= 1; i-- {
+		p *= i
+	}
+
+	return p
+}
+
+// AnotherSum takes an integer n as input and returns the sum of the first n positive integers, using a for loop
+func AnotherSum(n int) int {
+	if n < 0 {
+		panic("Error: negative input given to AnotherSum().")
+	}
+
+	sum := 0 // will store the final answer
+
+	for k := 1; k <= n; k++ {
+		sum += k
+	}
+
+	return sum
+}
+
+func GaussSum(n int) int {
+	return n * (n + 1) / 2
 }
 
 // mathematical fact: n! = n * (n-1)!
 // when n = 1: 1! = 1 * 0!
 // so 1 = 1 * 0! and therefore 1 = 0!
 
-//Factorial takes as input an integer n and returns n! = n * (n - 1) * (n - 2) * ... * 2 * 1
+// Factorial takes as input an integer n and returns n! = n * (n - 1) * (n - 2) * ... * 2 * 1
 func Factorial(n int) int {
 	if n < 0 {
 		panic("Error: negative input given to Factorial().") //Go's error message function
@@ -54,6 +147,9 @@ func Factorial(n int) int {
 		p = p * i
 		i = i + 1
 	}
+
+	// i is still in scope (its scope is the function)
+	fmt.Println("i is currently", i)
 
 	return p
 }
