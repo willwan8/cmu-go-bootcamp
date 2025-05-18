6654377ce1cc900eba13389fb82fcb8c8aa65e3c
willwan8 <williamwang1562@gmail.com>
2025-05-17 23:38:15 -0500

    completed While Loops in Go code along

---
+++ src/loops/main.go
@@ -0,0 +1,59 @@
+/*
+// Helpful pseudocode for this lesson
+Factorial(n)
+  p ← 1
+  i ← 1
+  while i ≤ n
+    p ← p · i
+    i ← i + 1
+  return p
+
+EuclidGCD(a,b)
+  while a ≠ b
+    if a > b
+      a ← a − b
+    else
+      b ← b − a
+  return a 
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Loops!")
+
+	fmt.Println(Factorial(5))
+
+	fmt.Println(Factorial(0))
+
+	// these factorials are so big that it results in integer overflow, so we get unexpected values!
+	fmt.Println(Factorial(40))
+	fmt.Println(Factorial(45))
+	fmt.Println(Factorial(300))
+}
+
+// mathematical fact: n! = n * (n-1)!
+// when n = 1: 1! = 1 * 0!
+// so 1 = 1 * 0! and therefore 1 = 0!
+
+//Factorial takes as input an integer n and returns n! = n * (n - 1) * (n - 2) * ... * 2 * 1
+func Factorial(n int) int {
+	if n < 0 {
+		panic("Error: negative input given to Factorial().") //Go's error message function
+	}
+
+	p := 1 // will store product
+	i := 1 // serves as a counter
+
+	// Go has no keyword "while" and uses "for" instead
+	for i <= n {
+		p = p * i
+		i = i + 1
+	}
+
+	return p
+}
