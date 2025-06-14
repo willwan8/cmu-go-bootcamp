cb4c61036583f38dfcd97df12803ba509e1d814f
willwan8 <williamwang1562@gmail.com>
2025-06-14 00:59:05 -0500

    completed simulating craps with monte carlo simulation code along

--- src/chapter2/dice/main.go
+++ src/chapter2/dice/main.go
@@ -16,6 +16,55 @@ func main() {
 	fmt.Println(rand.Float64()) // print decimal in range [0, 1)
 
 	fmt.Println(SumDice(2))
+
+	numTrials := 10000000
+
+	fmt.Println("Estimated house edge with", numTrials, "trials is:", ComputeCrapsHouseEdge(numTrials))
+}
+
+// PlayCrapsOnce simulates one game of craps.
+// Input: none
+// Output: true if the game is a win and false if it's a loss
+func PlayCrapsOnce() bool {
+	firstRoll := SumDice(2)
+	if firstRoll == 7 || firstRoll == 11 {
+		// winner!
+		return true
+	} else if firstRoll == 2 || firstRoll == 3 || firstRoll == 12 {
+		return false // loser! :(
+	} else {
+		// keep rolling until you hit a 7 or your original roll
+		for { // while forever
+			newRoll := SumDice(2)
+			if newRoll == firstRoll {
+				return true // winner!
+			} else if newRoll == 7 {
+				return false // loser! :(
+			}
+		}
+	}
+}
+
+// ComputeCrapsHouseEdge estimates the "house edge" of craps over multilpe simulations.
+// Input: an integer corresponding to the number of simulations.
+// Output: house edge of craps (average amount won or lost over the number of simulations per unit of currency)
+func ComputeCrapsHouseEdge(numTrials int) float64 {
+	count := 0 // will keep track of amount wont (+) or lost (-)
+
+	// run n trials and update count accordingly
+	for i := 0; i < numTrials; i++ {
+		// play the game
+		outcome := PlayCrapsOnce()
+		if outcome {
+			// winner!
+			count++
+		} else {
+			// loser! :(
+			count--
+		}
+	}
+
+	return float64(count) / float64(numTrials)
 }
 
 // RollDie
