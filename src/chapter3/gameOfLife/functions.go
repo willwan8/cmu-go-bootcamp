3f0b09332fb31d6e709429cd672ceb89b3c18099
willwan8 <williamwang1562@gmail.com>
2025-06-18 22:09:31 -0500

    completed implementing the game of life code along from website

--- src/chapter3/gameOfLife/functions.go
+++ src/chapter3/gameOfLife/functions.go
@@ -1,1 +1,98 @@
 package main
+
+// PlayGameOfLife takes as input an initial game board and a number of generations.
+// It returns a slice of game boards of length numGens+1 corresponding to playing
+// the Game of Life over numGens generations, starting with the initial board.
+func PlayGameOfLife(initialBoard GameBoard, numGens int) []GameBoard {
+	boards := make([]GameBoard, numGens+1)
+	boards[0] = initialBoard
+
+	for i := 1; i <= numGens; i++ {
+		boards[i] = UpdateBoard(boards[i-1])
+	}
+
+	return boards
+}
+
+// UpdateBoard takes as input a GameBoard.
+// It returns the board resulting from playing the Game of Life for one generation.
+func UpdateBoard(currentBoard GameBoard) GameBoard {
+	// first, create new board corresponding to the new generation
+	numRows := CountRows(currentBoard)
+	numCols := CountColumns(currentBoard)
+	newBoard := InitializeBoard(numRows, numCols)
+
+	// range through all cells of currentBoard and update each one.
+	for r := range currentBoard {
+		// range over values in currentBoard[r], the current row
+		for c := range currentBoard[r] {
+			// curr value is currentBoard[r][c]
+			newBoard[r][c] = UpdateCell(currentBoard, r, c)
+		}
+	}
+
+	return newBoard
+}
+
+// InitializeBoard takes as input a number of rows and a number of columns.
+// It returns a numRows x numCols GameBoard object.
+func InitializeBoard(numRows, numCols int) GameBoard {
+	b := make(GameBoard, numRows)
+	for i := range b {
+		b[i] = make([]bool, numCols)
+	}
+
+	return b
+}
+
+// UpdateCell takes a GameBoard and row/col indices r and c, and it returns the state of the board at these row/col indices is in the next generation
+func UpdateCell(board GameBoard, r, c int) bool {
+	numNeighbors := CountLiveNeighbors(board, r, c)
+
+	// now it's just a matter of consulting the rules.
+	if board[r][c] == true { // alive
+		if numNeighbors == 2 || numNeighbors == 3 {
+			return true // staying alive
+		} else {
+			return false // dying out
+		}
+	} else { // dead
+		if numNeighbors == 3 { // zombie!
+			return true
+		} else { //RIP
+			return false
+		}
+	}
+}
+
+// CountLiveNeighbors takes as input a GameBoard board along with row and column indices r, c.
+// It returns the sum of live neighbors of board[r][c], not including cells that fall off the boundaries of the board.
+func CountLiveNeighbors(board GameBoard, r, c int) int {
+	count := 0
+
+	// range over all cells in the neighborhood
+	for i := r - 1; i <= r+1; i++ {
+		for j := c - 1; j <= c+1; j++ {
+			// ensure (i, j) isn't (r, c), is on board, and is true
+			if (i != r || j != c) && InField(board, i, j) && board[i][j] {
+				// we found a live one!
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+// InField takes a GameBoard board as well as row and col indices (i, j).
+// It returns true if board[i][j] is in the board and false otherwise.
+func InField(board GameBoard, i, j int) bool {
+	if i < 0 || j < 0 {
+		return false
+	}
+	if i >= CountRows(board) || j >= CountColumns(board) {
+		return false
+	}
+	// if we survive to here, then we are on the board
+	return true
+}
--- src/chapter3/gameOfLife/main.go
+++ src/chapter3/gameOfLife/main.go
@@ -2,19 +2,78 @@ package main
 
 import (
 	"fmt"
-	"image/png" // for drawing an image to PNG
-	"os"        // for opening a file that will hold the image
+	// "image/png" // for drawing an image to PNG
+	"chapter3/gameOfLife/gifhelper"
+	"os" // for opening a file that will hold the image
+	"strconv"
 )
 
 func main() {
 	fmt.Println("Coding the Game of Life!")
+
+	// when we run a program, the command line arguments (CLAs) go into an array called os.Args
+
+	// we want to dictate that the length of the array is 5
+	if len(os.Args) != 5 {
+		panic("Incorrect number of command line parameters given. Must be 4.")
+	}
+
+	initialBoardFile := os.Args[1] // starting GameBoard file name
+	outputFile := os.Args[2]       // where to draw the animated GIF
+
+	// how many pixels wide should each cell be?
+	cellWidth, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		panic("Error: Problem parsing cell width parameter.")
+	}
+
+	// how many generations to play the automaton?
+	numGens, err2 := strconv.Atoi(os.Args[4])
+	if err2 != nil {
+		panic("Error: Problem parsing number of generations parameter.")
+	}
+
+	fmt.Println("Parameters read in successfully!")
+
+	initialBoard := ReadBoardFromFile(initialBoardFile)
+
+	fmt.Println("Playing the automaton.")
+
+	boards := PlayGameOfLife(initialBoard, numGens)
+
+	fmt.Println("Game of Life played. Now, drawing images.")
+
+	// we need a slice of image objects
+	imglist := DrawGameBoards(boards, cellWidth)
+	fmt.Println("boards drawn to images! Convert to animated GIF.")
+
+	// convert imagse to a GIF
+	gifhelper.ImagesToGIF(imglist, outputFile)
+
+	fmt.Println("Success! GIF produced.")
+
+	// drawing multiple boards of the game of life
 	/*
-			rPentomino := ReadBoardFromFile("boards/rPentomino.csv")
+				rPentomino := ReadBoardFromFile("boards/rPentomino.csv")
+
+				cellWidth := 20
+				img := DrawGameBoard(rPentomino, cellWidth)
+
+				filename := "output/rPentomino.png"
+
+				f, err := os.Create(filename)
+				Check(err)
+				defer f.Close()
+
+				err = png.Encode(f, img)
+				Check(err)
+
+			dinnerTable := ReadBoardFromFile("boards/dinnerTable.csv")
 
 			cellWidth := 20
-			img := DrawGameBoard(rPentomino, cellWidth)
+			img := DrawGameBoard(dinnerTable, cellWidth)
 
-			filename := "output/rPentomino.png"
+			filename := "output/dinnerTable.png"
 
 			f, err := os.Create(filename)
 			Check(err)
@@ -23,12 +82,12 @@ func main() {
 			err = png.Encode(f, img)
 			Check(err)
 
-		dinnerTable := ReadBoardFromFile("boards/dinnerTable.csv")
+		gosperGun := ReadBoardFromFile("boards/gospergun.csv")
 
 		cellWidth := 20
-		img := DrawGameBoard(dinnerTable, cellWidth)
+		img := DrawGameBoard(gosperGun, cellWidth)
 
-		filename := "output/dinnerTable.png"
+		filename := "output/gospergun.png"
 
 		f, err := os.Create(filename)
 		Check(err)
@@ -37,18 +96,4 @@ func main() {
 		err = png.Encode(f, img)
 		Check(err)
 	*/
-
-	gosperGun := ReadBoardFromFile("boards/gospergun.csv")
-
-	cellWidth := 20
-	img := DrawGameBoard(gosperGun, cellWidth)
-
-	filename := "output/gospergun.png"
-
-	f, err := os.Create(filename)
-	Check(err)
-	defer f.Close()
-
-	err = png.Encode(f, img)
-	Check(err)
 }
